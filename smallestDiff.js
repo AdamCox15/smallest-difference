9a8c7b91c6672a0b31111115d3af5336010a58ab
Adam Cox <adamcox.dev@gmail.com>
2022-09-28 10:25:32 -0600

    Final commit with solution

--- smallestDiff.js
+++ smallestDiff.js
@@ -11,3 +11,24 @@
 // The result would be 3, since 23 - 20 = 3 is the smallest difference of any pair of numbers in those lists.
 
 // Write a function that determines the smallest difference.
+
+function smallestDifference(arr1, arr2){
+    if(arr1.length === 0 || arr2.length === 0){ return -1; }
+  
+    let result = Number.MAX_SAFE_INTEGER; // (2^53) - 1
+    // 9007199254740991
+  
+    for(let i = 0; i < arr1.length; i++){
+      for(let j = 0; j < arr2.length; j++){
+        //Math.abs meaning return the positive not negative
+        if(Math.abs(arr1[i] - arr2[j]) < result){
+          result = Math.abs(arr1[i] - arr2[j]);
+        }
+      }
+    }
+    return result;
+  }
+  
+  console.log(smallestDifference([10, 20, 14, 16, 18],[30, 23, 54, 33, 96]))
+
+  //--------------------------------------------------------------------------
